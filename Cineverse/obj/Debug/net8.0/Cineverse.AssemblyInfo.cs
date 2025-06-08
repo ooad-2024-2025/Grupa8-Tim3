3b9b7ae1f9e5b2f5fd8324a08099a0ead855b327
ekurtovic5 <emirakurtovic2003@gmail.com>
2025-06-08 15:22:16 +0200

    Homepage

--- Cineverse/obj/Debug/net8.0/Cineverse.AssemblyInfo.cs
+++ Cineverse/obj/Debug/net8.0/Cineverse.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cineverse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92db6898b997c5ac1762b22ad81cda046435cb11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903026db018701f57136cf3afede31526d966359")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cineverse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cineverse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
